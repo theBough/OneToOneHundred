801ff7707a27ac6d65822f0de7f84988c33bd410
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-01-12 13:48:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var rndNumbers = [];
+
 function makeButtons(){
-  document.getElementById("title").innerHTML = "...";
-}
+  makeARndList();
+  for (var i =0 ; i<100 ; i++ ){
+    var btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", "btnClass");
+    btn.setAttribute("id", i);
+    document.getElementById("theGrid").appendChild(btn);
+    document.getElementById(i).innerHTML = rndNumbers[i];
+    
+  }//end for loop
+}//end Function
+
+function makeARndList(){
+    while(rndNumbers.length<100){
+        var r = Math.floor(Math.random() * 100)+1;
+        if(!rndNumbers.includes(r)){
+            rndNumbers.push(r);
+        }//end if
+    }//end loop
+    console.log(rndNumbers);
+}//end function
+

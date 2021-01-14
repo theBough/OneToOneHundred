f0d9f695cdf9063e78e0bce11eac79a8a837b619
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-01-14 13:41:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,33 @@
-var rndList = [];
-var targetNumber = 1;
-var minutes = 0;
-var seconds = 0;
-var timer;
+var rndNumbers = [];
+var target  =1;
 
 function makeButtons(){
-  makeRndList();
-  for( var i = 0 ; i<100 ; i++){
+  makeARndList();
+  for (var i =0 ; i<100 ; i++ ){
     var btn = document.createElement("button");
-    btn.setAttribute("type","button");
-    btn.setAttribute("class","btnClass");
-    btn.setAttribute("id",i);
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", "btnClass");
+    btn.setAttribute("id", i);
     document.getElementById("theGrid").appendChild(btn);
-    document.getElementById(i).innerHTML = rndList[i];
+    document.getElementById(i).innerHTML = rndNumbers[i];
     document.getElementById(i).onclick = thisClicked;
   }//end for loop
-  timer = setInterval(clock,1000);
-  document.getElementById("btnStart").style.display = "none";
-}//end function makeButtons
-function makeRndList(){
-  while(rndList.length < 100){
-    var rnd  = Math.floor( Math.random() *100 ) +1;
-    if(rndList.indexOf(rnd)==  -1 )rndList.push(rnd);
-  }//end while
-}//end makeRndList
-var thisClicked = function(){
-  if(document.getElementById(this.id).innerHTML == targetNumber){
-    targetNumber +=1;
-    document.getElementById(this.id).innerHTML = "";
-    document.getElementById("HTMLActiveNumber").innerHTML = "Current number: "+ targetNumber;
-    if(targetNumber>100){
-      clearInterval(timer);
-      document.getElementById("HTMLActiveNumber").innerHTML = "Way to go you won!";
+}//end Function
+var thisClicked = function() {
+    if(document.getElementById(this.id).innerHTML == target){
+        target += 1;
+        document.getElementById("htmlTarget").innerHTML = "You are on number: " + target;
+        document.getElementById(this.id).innerHTML = "";
     }//end if
-  }//end if
-}//end function ThisClicked
-function clock(){
-  seconds +=1;
-  if (seconds > 59){
-    minutes +=1;
-    seconds = 0;
-  }
-  if (seconds < 10){
-    document.getElementById("clock").innerHTML = minutes +":0"+seconds;
-  }else{
-    document.getElementById("clock").innerHTML = minutes +":"+seconds;
-  }
-  
-}//end Clock function
+}//end function
+
+function makeARndList(){
+    while(rndNumbers.length<100){
+        var r = Math.floor(Math.random() * 100)+1;
+        if(!rndNumbers.includes(r)){
+            rndNumbers.push(r);
+        }//end if
+    }//end loop
+    console.log(rndNumbers);
+}//end function
+
